a9ff246f93de3b07aece19f0bd89d197d5e34182
rubinsingh1200 <55121655+rubinsingh1200@users.noreply.github.com>
2021-01-15 14:41:43 +0545

    home page

--- app/src/main/java/com/example/week6assignment1/Home_Activity.kt
+++ app/src/main/java/com/example/week6assignment1/Home_Activity.kt
@@ -1,5 +1,7 @@
 package com.example.week6assignment1
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,9 @@ import androidx.navigation.ui.setupWithNavController
 
 class Home_Activity : AppCompatActivity() {
 
+    private var storage =  Storage()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_)
@@ -18,12 +23,28 @@ class Home_Activity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_addStudent, R.id.navigation_aboutUs
-            )
-        )
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_addStudent, R.id.navigation_aboutUs))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+    override fun onBackPressed() {
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout?")
+
+        builder.setMessage("Do you want to logout?")
+
+        builder.setIcon(android.R.drawable.ic_dialog_info)
+
+        builder.setPositiveButton("YES"){ _, _ ->
+            storage.setLoggedIn(null)
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("msg", "Logged Out")
+            startActivity(intent)
+        }
+
+        val alert: AlertDialog = builder.create()
+        alert.setCancelable(true)
+        alert.show()
     }
 }
--- app/src/main/java/com/example/week6assignment1/fragments/fragment_addstudent.kt
+++ app/src/main/java/com/example/week6assignment1/fragments/fragment_addstudent.kt
@@ -1,58 +1,109 @@
 package com.example.week6assignment1.fragments
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 import com.example.week6assignment1.R
+import com.example.week6assignment1.Storage
+import com.example.week6assignment1.model.student
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMB
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_addstudent.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment_addstudent : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var btnSave: Button
+    private lateinit var etFullname: EditText
+    private lateinit var etAge: EditText
+    private lateinit var etImageURL: EditText
+    private lateinit var etAddress: EditText
+    private lateinit var rbMale: RadioButton
+    private lateinit var rbFemale: RadioButton
+    private lateinit var rbOthers: RadioButton
+    private var userImageURL = ""
+    private var gender = "Not Specified"
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_addstudent, container, false)
+        val view = inflater.inflate(R.layout.fragment_addstudent, container, false)
+        etFullname = view.findViewById(R.id.etFullname)
+        etAge = view.findViewById(R.id.etAge)
+        etImageURL = view.findViewById(R.id.etImageURL)
+        etAddress = view.findViewById(R.id.etAddress)
+        rbMale = view.findViewById(R.id.rbMale)
+        rbFemale = view.findViewById(R.id.rbFemale)
+        rbOthers = view.findViewById(R.id.rbOthers)
+        btnSave = view.findViewById(R.id.btnSave)
+
+        rbMale.setOnClickListener {
+            gender = "Male"
+        }
+        rbFemale.setOnClickListener {
+            gender = "Female"
+        }
+        rbOthers.setOnClickListener {
+            gender = "Others"
+        }
+
+        btnSave.setOnClickListener {
+            if(validateInput()) {
+                var fullname = etFullname.text.toString()
+                userImageURL = etImageURL.text.toString()
+                var age = etAge.text.toString()
+                var address = etAddress.text.toString()
+                Storage().appendStudent(student(fullname, userImageURL, age, address, gender))
+                Toast.makeText(view?.context, "Student Added Sucessfully", Toast.LENGTH_LONG).show()
+                etFullname.setText("")
+                etImageURL.setText("")
+                etAddress.setText("")
+                etAge.setText("")
+                etFullname.requestFocus()
+            }else{
+                Toast.makeText(view?.context, "Try Again", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return view
+    }
+    private fun validateInput(): Boolean {
+        var res : Boolean = true
+        when {
+            TextUtils.isEmpty(etFullname.text) -> {
+                etFullname.error = "This field should not be empty"
+                etFullname.requestFocus()
+                res = false
+            }
+            TextUtils.isEmpty(etImageURL.text) -> {
+                etImageURL.error = "This field should not be empty"
+                etImageURL.requestFocus()
+                res = false
+            }
+            TextUtils.isEmpty(etAddress.text) -> {
+                etAddress.error = "This field should not be empty"
+                etAddress.requestFocus()
+                res = false
+            }
+            TextUtils.isEmpty(etAge.text) -> {
+                etAge.error = "This field should not be empty"
+                etAge.requestFocus()
+                res = false
+            }
+        }
+
+        return res
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_addstudent.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                fragment_addstudent().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
 }
